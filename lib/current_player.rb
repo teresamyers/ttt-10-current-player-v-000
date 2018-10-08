17733dce7508474d11cea1f7bcfdea16a14385b9
teresa myers <teresamarie04@gmail.com>
2018-10-08 01:46:29 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,24 +1,17 @@
 def turn_count (board)
-  counter = 0
-  board.each do |condition|
-    if condition == "" || condition == " "
-    else
-      counter += 1
+  count = 0
+  board.each do |turn|
+    if turn == "" || turn == " "
+      count += 1
     end
   end
-  counter
+  count
 end
 
 def current_player(board)
-  num_moves = turn_count(board)
-  =begin
-  if num_moves % 2 == 0
-    current_player = "X"
+  if turn_count(board) % 2 == 0
+    "X"
   else
-    current_player = "O"
+    "O"
   end
-  =end
-  moves % 2 == 0 ? current_player = "X" : current_player = "O"
-
-  current_player
 end
